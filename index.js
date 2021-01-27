f87ef1d810641b99541389d59a6193c0f5e25ee5
Alamin Mirdha <mdalaminmirdha1211@gmail.com>
2021-01-28 03:40:27 +0600

    added something

---
+++ index.js
@@ -0,0 +1,5 @@
+const loginbtn = document.getElementById('login');
+loginbtn.addEventListener('click', function () {
+    const loginArea = document.getElementById('login-area');
+    loginArea.style.display = 'none';
+})
